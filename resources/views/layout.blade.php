4c916438a4e057bbc7d4f27da27699a3c3695e5f
Dant Benjamin <sukkhato@gmail.com>
2016-02-11 13:59:59 +0100

    première migration de la base de données

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -128,7 +128,7 @@
   <div class="bh-copy">&copy; 2016 Simplon.co Midi-Pyrénées </div>
 
 <!-- chargement des scripts -->
-<script src="/js/app.js"></script>
+<script src="./js/app.js"></script>
 
 <script type="text/javascript">
   WebFontConfig = {
