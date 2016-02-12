534b468826fc54019833161ca3dec52686d320f5
hervé <perezherve310gmail.com>
2016-02-12 11:14:44 +0100

    modification page partenaire
    application du nouveau style à la page partenaire

--- resources/views/pages/partenaires.blade.php
+++ resources/views/pages/partenaires.blade.php
@@ -2,98 +2,60 @@
 
 @section('content')
 
-<div class="ui stackable padded segment equal width"><h1 class="ui center aligned header">Partenaires</h1>
-</div>
+<h1 class="ui one column center aligned header">Partenaires</h1>
 <div class="ui container">
-    <div class="ui vertically divided container stackable grid">
-      <div class="ui two column row">
-        <div class="sixteen wide tablet twelve wide computer column ">
-          Devenir partenaire de Simplon-MIP c’est faire le parti de l’#openinnovation et de l’apprentissage permanent. <br>
-          Nous nous développons avec des entreprises qui nous font confiance et  souhaitent faire le pari de l’innovation partagée. <br>
-          <br>
-          Associez-vous à une marque puissante et référente dans les univers tech – innovation sociale – diversité/inclusion, et à sa “touch” spécifique pour mener des actions qui ont du sens, et profitez :
-          <br> <br>
-          #d’une approche technique autant vulgarisée qu’utile <br>
-          #des coûts maîtrisés pour des actions qui ont un sens pédagogique et social <br>
-          #d’une utilisation de la diversité (âges, origines, approches) au service de l’innovation (tech, sociale et RH)
-​
-        </div>
-        <div class="eight wide centered tablet four wide computer column ">
-          <div class="ui attached segment">
-            <p>Vous voulez devenir partenaire de Simplon.co et développer avec nous l'innovation pour tous ?</p>
-          </div>
-          <a class="huge ui red bottom attached button" tabindex="0" href="mailto:xxxxxxxxx@gmail.com
-          ?subject=objet&body=Bonjour" id="partenaire">Ecrivez-nous!</a>
-        </div>
-​
-       
+  <div class="ui vertically divided container stackable grid">
+    <div class="ui two column row">
+      <div class="sixteen wide tablet twelve wide computer column ">
+       <p> Devenir partenaire de Simplon-MIP c’est faire le parti de l’#openinnovation et de l’apprentissage permanent. <br>
+        Nous nous développons avec des entreprises qui nous font confiance et  souhaitent faire le pari de l’innovation partagée. <br>
+        <br>
+        Associez-vous à une marque puissante et référente dans les univers tech – innovation sociale – diversité/inclusion, et à sa “touch” spécifique pour mener des actions qui ont du sens, et profitez :
+        <br> <br>
+        #d’une approche technique autant vulgarisée qu’utile <br>
+        #des coûts maîtrisés pour des actions qui ont un sens pédagogique et social <br>
+        #d’une utilisation de la diversité (âges, origines, approches) au service de l’innovation (tech, sociale et RH)
+        ​</p>
       </div>
-   
-      <h2 class="ui header">Tous les jours ils font SimplonMIP avec nous
-      </h2>
-      
-      <div class="ui three column row">
-        <div class="column">
-          <div class="ui centered card ">
-            <div style="background:url('img/logo-region.png') no-repeat center"class="backgroundPartenaire">
-            </div>
-            <div class="content">
-              <a class="header" href="http://www.midipyrenees.fr">Conseil Régional Midi-Pyrénées </a>
-              <p> 
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="column">
-          <div class="ui centered card">
-            <div style="background:url('img/logo-er2c.png') no-repeat center"class="backgroundPartenaire">
-            </div>
-            <div class="content">
-              <a class="header" href="er2c-mip.com">Ecole de la 2ème chance</a>
-              <p>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="column">
-          <div class="ui centered card">
-            <div style="background:url('img/logo-crv.png') no-repeat center"class="backgroundPartenaire">
-            </div>
-            <div class="content">
-              <a class="header" href="rousselot-voltaire.com">Cours Rousselot Voltaire</a>
-              <p>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>  
-​
-      <div class="ui two column row">
-        <div class="column">
-          <div class="ui centered card ">
-            <div style="background:url('img/logo-lamelee.png') no-repeat center"class="backgroundPartenaire">
-            </div>
-            <div class="content">
-              <a class="header" href="http://www.lamelee.com/">la Mêlée </a>
-              <p>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="column">
-          <div class="ui centered card">
-            <div style="background:url('img/logo-etincelle-coworking.png') no-repeat center"class="backgroundPartenaire">
-            </div>
-            <div class="content">
-              <a class="header" href="http://www.coworking-toulouse.com/">Etincelle Coworking</a>
-              <p>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>  
-    
+
+      <div class="eight wide centered tablet four wide computer column ">
+        <p>Vous voulez devenir partenaire de Simplon.co et développer avec nous l'innovation pour tous ?</p>
+        <a class="huge ui red bottom attached button" tabindex="0" href="mailto:xxxxxxxxx@gmail.com
+        ?subject=objet&body=Bonjour" id="partenaire">Ecrivez-nous!</a>
+      </div>
+    </div>  ​
   </div>
+
+  <div><h2 class="ui header">Tous les jours ils font SimplonMIP avec nous</h2>
+  </div>  
+  <div class="ui contener ">
+    <div class="ui three column grid ">
+      <div class="column "> 
+        <div class="ui small image">
+          <img src="/img/logo-crv.png" class="backgroundPartenaire" alt="logo cours Rousselot Voltaire">
+          <div class="content">
+            <a class="header"href="rousselot-voltaire.com">Cours Rousselot Voltaire</a>
+          </div>
+        </div>
+      </div>
+      <div class="column ph-centre" >
+        <div class="ui small image" >
+          <img src="/img/logo-lamelee.png" class="backgroundPartenaire" alt="logo La Melee">
+          <div class="content">
+            <a class="header" href="http://www.lamelee.com/">la Mêlée </a>
+          </div>
+        </div>
+      </div>
+      <div class="column ph-droit">
+        <div class="ui small image">
+          <img src="/img/logo-etincelle-coworking.png" class="backgroundPartenaire " alt="logo Etincelle Coworking">
+          <div class="content">
+            <a class="header" href="http://www.coworking-toulouse.com/">Etincelle Coworking</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
 </div>
 
 @endsection
